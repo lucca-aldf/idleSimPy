2b3e9e239e8e4155f9640ac9ee1d9c91ed884194
Lucca Limongi <laldf@cin.ufpe.br>
2023-04-04 10:27:29 -0300

    Correção de variavel jogador

--- core/aventura.py
+++ core/aventura.py
@@ -10,18 +10,18 @@ def aventura(_jogador, _dificuldade):
         
         if _jogador.vivo:
             print("Quest terminada!")
-            jogador.historia.append(f"{jogador.nome} terminou sua quest.")
+            _jogador.historia.append(f"{_jogador.nome} terminou sua quest.")
 
             _dificuldade += 1
             _jogador.dano *= 1.5
             _jogador.vida_max *= 1.6
             _jogador.vida = _jogador.vida_max
 
-            print(f"{jogador.nome} é agora nível {_dificuldade}")
-            jogador.historia.append(f"{jogador.nome} é um aventureiro de nível {_dificuldade} agora.")
+            print(f"{_jogador.nome} é agora nível {_dificuldade}")
+            _jogador.historia.append(f"{_jogador.nome} é um aventureiro de nível {_dificuldade} agora.")
         
         else:
-            print(f"{jogador.nome} faleceu...")
-            jogador.historia.append(f"{jogador.nome} foi derrotado em combate.")
-            jogador.historia.append(f"Esse é o fim da história de {jogador.nome}")
-    print(jogador.historia)
+            print(f"{_jogador.nome} faleceu...")
+            _jogador.historia.append(f"{_jogador.nome} foi derrotado em combate.")
+            _jogador.historia.append(f"Esse é o fim da história de {_jogador.nome}")
+    print(_jogador.historia)
